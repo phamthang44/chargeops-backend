629b24a23fbd3009a7341cd12535159194334010
phamthang44 <Ducthang4342@gmail.com>
2026-07-23 10:55:25 +0700

    project initialization

---
+++ src/main/java/com/thang/chargeops/ChargeopsApplication.java
@@ -0,0 +1,13 @@
+package com.thang.chargeops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thang/chargeops/ChargeopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thang.chargeops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
