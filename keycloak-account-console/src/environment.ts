f0110751e00758e7f45da873eb6dee8829d1abd5
phamthang44 <Ducthang4342@gmail.com>
2026-08-15 19:43:04 +0700

    chore(keycloak): relocate account console to frontend workspace

--- keycloak-account-console/src/environment.ts
+++
@@ -1,4 +0,0 @@
-import { AccountEnvironment } from "@keycloak/keycloak-account-ui";
-import { getInjectedEnvironment } from "@keycloak/keycloak-ui-shared";
-
-export const environment = getInjectedEnvironment<AccountEnvironment>();
